357a13825e8b52d449199bedd1499684c3b35d00
Zedrich <zedrichzaivan753@gmail.com>
2026-02-10 20:55:10 +0800

    Initial commit: Fruit Catcher Game

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() {
     runApp(const MyApp());
   });
 }
-//hello zedrichhhh
+//hello zedrichhhhfdsfsdfds
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
